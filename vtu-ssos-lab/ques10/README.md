d5300b896e4ba9554b5f8846fd2b55722732132a
sujith <sujithvm24@gmail.com>
2014-11-20 13:08:17 +0530

    Update scheduling program

--- vtu-ssos-lab/ques10/README.md
+++ vtu-ssos-lab/ques10/README.md
@@ -1,20 +1,95 @@
 Problem Statement
 =================
 
-C program to do the following using fork() create a child process. The child process prints its own process id and id of its parent 
-and then exits. The parent process waits for its child to finish and prints its own process id and the id of its child process
-and then exits.
+Design, develop and execute a program in C/C++ to simulate the working of Shortest Remaining Time and Round-Robin
+Scheduling Algorithms. Experiment different quantum sizes for the Round-Robin algorithm.
+In all cases, determing the average turn around time. The input can be read from keyboard or from a file.
 
 ### Compilation
 
 ```
 
-$ gcc ques9b.c
+$ gcc ques10.c
 $ ./a.out 
-Parent process
-My id = 6886, child id = 6887
-Child process
-My id = 6887, parent id = 6886
+
+Select type of scheduling
+1. SJF
+2. RR
+3. EXIT
+
+Enter your choice: 1
+
+
+Shortest job first scheduling
+
+Enter the total number of procedures: 4
+
+Process : 1
+Enter procedure name: 0
+Enter arrival time: 0
+Enter burst time of procedure: 8
+
+Process : 2
+Enter procedure name: 1
+Enter arrival time: 1
+Enter burst time of procedure: 4
+
+Process : 3
+Enter procedure name: 2
+Enter arrival time: 2
+Enter burst time of procedure: 9
+
+Process : 4
+Enter procedure name: 3
+Enter arrival time: 3
+Enter burst time of procedure: 5
+
+
+Name	Arrival	Burst	Wait	Turn_Around_Time
+
+P0	0	8	9	17
+P1	1	4	0	4
+P2	2	9	15	24
+P3	3	5	2	7
+
+Total wait time = 26
+Total turn around time = 52
+Average wait time = 6.500000
+Average turn around time = 13.000000
+
+```
+
+Run #2
+
+```
+
+Select type of scheduling
+1. SJF
+2. RR
+3. EXIT
+
+Enter your choice: 2
+
+
+Round robin scheduling
+
+Enter number of processes: 3
+
+Enter burst time for them
+24
+3
+3
+Enter time quantum
+4
+
+
+Process	Burst	Wait	Turn-Around-Time
+1	24	6	30
+2	3	4	7
+3	3	7	10
+
+Average waiting time = 5.666667
+Average turn around time = 15.666667
 
 
 ```
--- vtu-ssos-lab/ques10/ques10.c
+++ vtu-ssos-lab/ques10/ques10.c
@@ -1,190 +1,198 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PROCESSES 5
+
 typedef struct
 {
-	int name;
-	int at;
-	int burst;
-	int wait;
-	int TAT;
-	int remaining;
-}process;
+	int name;	// name of process
+	int at;		// arrival time
+	int burst;	// burst time
+	int wait;	// wait time
+	int TAT;	// turn around time
+	int remaining;	// remaining time
+} process;
 
-process p[5], p1[5];
+process p[MAX_PROCESSES];		// shortest job first processes
+process p1[MAX_PROCESSES]; 		// round robin processes
 
-int n, n1;
+int n;			// number of processes in shortest job first
+int n1;			// number of processes in round robin
 
-void round_robin();
-void accept();
-void sjf();
-int minimum();
+void sjf();		// shortest job first scheduling
+void round_robin();	// round robin scheduling
 
-int tq, t, i, count = 0, rtwt = 0, rttat = 0, temp, sq = 0, stwt = 0, sttat = 0;
-float rawt = 0.0, ratat = 0.0, sawt = 0.0, satat = 0;
+int rtwt = 0;		// round robin - total wait time
+int rttat = 0; 		// round robin - total turn around time
+int stwt = 0;		// shortest job first - total wait time
+int sttat = 0;		// shortest job first - total turn around time
+
+float rawt = 0.0; 	// round robin - average wait time
+float ratat = 0.0;	// round robin - average turn around time
+float sawt = 0.0;	// shortest job first - average wait time 
+float satat = 0;	// shortest job first - average turn around time
+
+int tq, t, i, j, count = 0, temp, sq = 0;
 
 void main()
 {
 	int ch;
-	printf("\n1. SJF\n2. RR\n3. EXIT\n\n\tEnter your choice: ");
-	scanf("%d",&ch);
+	printf ("\nSelect type of scheduling\n1. SJF\n2. RR\n3. EXIT\n\nEnter your choice: ");
+	scanf ("%d",&ch);
 
 	switch(ch)
 	{
-		case 1:
-			accept();
+		case 1:			
 			sjf();
 			break;
 		case 2:
 			round_robin();
 			break;
 		case 3:
+			exit(0);
 			break;
 		default:
-			printf("Invalid choice!!\n");
-	}
-}
-
-void accept()
-{
-	int i;
-
-	printf("Enter the total number of procedures: ");
-	scanf("%d",&n);
-
-	for(i=0;i<n;i++)
-	{
-		printf("\nEnter procedure name: ");
-		scanf("%d",&p[i].name);
-
-		printf("\nEnter arrival time: ");
-		scanf("%d",&p[i].at);
-
-		printf("\nEnter burst time of procedure: ");
-		scanf("%d",&p[i].burst);
-
-		p[i].wait = 0;
+			printf ("Invalid choice!!\n");			
 	}
 }
 
 void sjf()
 {
-	int i, j, tbt = 0, no, k, min;
+	int tbt = 0, no, k, min;
 
-	for(i=0;i<n;i++)
+	printf ("\n\nShortest job first scheduling\n\n");
+	printf ("Enter the total number of procedures: ");
+	scanf ("%d",&n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf ("\nProcess : %d\n", (i + 1));
+		printf ("Enter procedure name: ");
+		scanf ("%d", &p[i].name);
+
+		printf ("Enter arrival time: ");
+		scanf ("%d", &p[i].at);
+
+		printf ("Enter burst time of procedure: ");
+		scanf ("%d", &p[i].burst);
+
+		p[i].wait = 0;
+	}	
+
+	for (i = 0; i < n; i++)
 	{
 		p[i].remaining = p[i].burst;
 		tbt = tbt + p[i].burst;
 	}
 	
-	for(i=0;i<n;i++)
+	for (i = 0; i < tbt; i++)
 	{
 		min = 999;
-		for(j=0;j<n;j++)
+		for (j = 0; j < n; j++)
 		{
-			if(p[j].at <= i && p[j].remaining > 0)
+			if (p[j].at <= i && p[j].remaining > 0)
 			{
-				if(p[j].remaining < min)
+				if (p[j].remaining < min)
 				{
 					min = p[j].remaining;
 					no = j;
 				}
 			}
 		}
-
-		printf("%d-p%d-",i,no);
+		
 		p[no].remaining = p[no].remaining - 1;
 
-		for(k=0;k<n;k++)
+		for (k = 0; k < n; k++)
 		{
-			if(p[k].at <= i && p[k].remaining > 0 && p[k].name != no)
+			if(p[k].at <= i && p[k].remaining > 0 && p[k].name != no) 
+			{
 				p[k].wait = p[k].wait + 1;
+			}
 		}
 	}
 
-	printf("%d",i);
-
-	for(k=0;k<n;k++)
+	for (k = 0; k < n; k++)
 	{
 		p[k].TAT = p[k].burst + p[k].wait;
 	}
 
-	printf("\nName\tArrival\tBurst\tWait\tTurn_Around_Time\n");
+	printf ("\n\nName\tArrival\tBurst\tWait\tTurn_Around_Time\n");
 
-	for(k=0;k<n;k++)
+	for (k = 0; k < n; k++)
 	{
 		stwt = stwt + p[k].wait;
 		sttat = sttat + p[k].TAT;
-		printf("\nP%d\t%d\t%d\t%d\t%d", p[k].name, p[k].at, p[k].burst, p[k].wait, p[k].TAT);
+		printf ("\nP%d\t%d\t%d\t%d\t%d", p[k].name, p[k].at, p[k].burst, p[k].wait, p[k].TAT);
 	}
-	sawt = (float)stwt/n;
-	satat = (float)sttat/n;
-	printf("\nstwt = %d\tsttat=%d\tsawt=%f\tsatat=%f\n", stwt, sttat, sawt, satat);
+	sawt = (float) stwt / n;
+	satat = (float) sttat / n;
+
+	printf ("\n\nTotal wait time = %d\nTotal turn around time = %d\n", stwt, sttat);
+	printf ("Average wait time = %f\nAverage turn around time = %f\n", sawt, satat);	
 }
 
 void round_robin()
 {
-	printf("Enter number of processes: ");
-	scanf("%d",&n1);
+	printf ("\n\nRound robin scheduling\n\n");
+	printf ("Enter number of processes: ");
+	scanf ("%d", &n1);
 
-	printf("\nEnter burst time for them\n");
-	for(i=0;i<n1;i++)
+	printf ("\nEnter burst time for them\n");
+	for (i = 0; i < n1; i++)
 	{
-		scanf("%d",&p1[i].burst);
+		scanf ("%d", &p1[i].burst);
 		p1[i].remaining = p1[i].burst;
 		p1[i].at = 0;
 	}
 
-	printf("Enter time quantum\n");
-	scanf("%d",&tq);
+	printf ("Enter time quantum\n");
+	scanf ("%d", &tq);
 	t = 0;
 
-	while(1)
+	while (1)
 	{
-		for(i=0,count=0;i<n1;i++)
+		for (i = 0, count = 0; i < n1; i++)
 		{
 			temp = tq;
-			if(p1[i].remaining == 0)
+			if (p1[i].remaining == 0)
 			{
 				count++;
 				continue;
 			}
 
-			if(p1[i].remaining > tq)
+			if (p1[i].remaining > tq)
 			{
-				p1[i].remaining = p1[i].remaining - tq;
-				printf("%d-",t);
-				t = t + tq;
-				printf("P%d-",i);
+				p1[i].remaining = p1[i].remaining - tq;				
+				t = t + tq;				
 			}
-			else
-				if(p1[i].remaining >= 0)
-				{
-					temp = p1[i].remaining;
-					printf("%d-",t);
-					t = t + p1[i].remaining;
-					printf("P%d-",i);
-				}
+			else if (p1[i].remaining >= 0)
+			{
+				temp = p1[i].remaining;				
+				t = t + p1[i].remaining;
+				p1[i].remaining = 0;				
+			}
 			sq = sq + temp;
 			p1[i].TAT = sq;
 		}
-		if(n1 == count)
+		if (n1 == count)
 			break;
-	}
-	printf("%d",t);
+	}	
 
-	for(i=0;i<n1;i++)
+	for (i = 0; i < n1; i++)
 	{
 		p1[i].wait = p1[i].TAT - p1[i].burst;
 		rtwt = rtwt + p1[i].wait;
 		rttat = rttat + p1[i].TAT;
 	}
-	rawt = (float)rtwt/n1;
-	ratat = (float)rttat/n1;
-	printf("\nProcess_no\tBurst_time\tWait_time\tTurn_Around_Time\n");
 
-	for(i=0;i<n1;i++)
+	rawt = (float) rtwt/n1;
+	ratat = (float) rttat/n1;
+	printf ("\n\nProcess\tBurst\tWait\tTurn-Around-Time\n");
+
+	for (i = 0; i < n1; i++)
 	{
-		printf("%d\t%d\t%d\t%d\n",i+1,p1[i].burst,p1[i].wait,p1[i].TAT);
+		printf ("%d\t%d\t%d\t%d\n", i + 1, p1[i].burst, p1[i].wait, p1[i].TAT);
 	}
 
-	printf("rawt = %f\tratat = %f\n", rawt, ratat);
+	printf ("\nAverage waiting time = %f\nAverage turn around time = %f\n", rawt, ratat);
 }
