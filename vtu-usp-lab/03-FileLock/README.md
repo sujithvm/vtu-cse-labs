593226b184b071730fed4aa51d0ddcf30c5053f2
sujith <sujithvm24@gmail.com>
2015-03-14 15:03:28 +0530

    update file lock code

---
+++ vtu-usp-lab/03-FileLock/README.md
@@ -0,0 +1,29 @@
+Problem Statement
+=================
+
+Consider the last 100 bytes as a region. Write a C/C++ program to check whether the region is locked or not. If the region is locked, print pid of the process which has locked. If the region is not locked, lock the region with an exclusive lock, read the last 50 bytes and unlock the region.
+
+
+### Compilation
+
+```
+
+$ g++ ques3.cpp 
+$ ./a.out test_file
+
+Setting lock...
+
+File locked...
+
+Last 100 bytes read from file : 
+ guardian, a watchful protector, a Dark Knight.
+
+
+Releasing lock...
+
+File unlocked...
+
+```
+
+
+
---
+++ vtu-usp-lab/03-FileLock/ques3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 256
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+	char buf[BUF_SIZE];
+	struct flock fvar;
+
+	if (argc != 2)
+	{
+		cout << "usage : " << argv[0] << " <file> " << endl;
+		exit(0);
+	}
+
+	if ((fd = open(argv[1], O_RDWR)) == -1)
+	{
+		cout << "Error : file open " << endl;
+		exit(1);
+	}
+
+	fvar.l_type = F_WRLCK;
+	fvar.l_whence = SEEK_END;
+	fvar.l_start = SEEK_END - 100;
+	fvar.l_len = 100;
+	
+	cout << "\nSetting lock..." << endl;
+	
+	if ((fcntl(fd, F_SETLK, &fvar)) == -1)
+	{     
+		fcntl(fd, F_GETLK, &fvar);
+		cout << "Error : File already locked by process with pid  : " << fvar.l_pid << endl;	
+		exit(1);	
+	}
+
+	cout << "\nFile locked..." << endl;
+
+	if ((lseek(fd, SEEK_END - 50, SEEK_END)) == -1)
+	{
+		cout << "Error : lseek " << endl;
+		exit(1);
+	}
+
+	if ((read(fd, buf, 100)) == -1)
+	{
+		cout << "Error : read " << endl;
+		exit(1);
+	}
+
+	cout << "\nLast 100 bytes read from file : " << endl;
+	cout << buf << endl;
+
+	cout << "\nReleasing lock..." << endl;
+	
+	fvar.l_type = F_UNLCK;
+	fvar.l_whence = SEEK_SET;
+	fvar.l_start = 0;
+	fvar.l_len = 0;
+
+	if ((fcntl(fd, F_UNLCK, &fvar)) == -1)
+	{
+		cout << "Error : unlock " << endl;
+		exit(1);
+	}
+
+	cout << "\nFile unlocked..." << endl;
+
+	close(fd);
+
+	return 0;
+ }
