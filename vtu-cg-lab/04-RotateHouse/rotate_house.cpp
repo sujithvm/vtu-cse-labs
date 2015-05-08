4b3ada7ae0eb52d52e38282515d32b4fbc12d2fd
sujith <sujithvm24@gmail.com>
2015-05-08 11:35:46 +0530

    Fix bug : convert to radians

--- vtu-cg-lab/04-RotateHouse/rotate_house.cpp
+++ vtu-cg-lab/04-RotateHouse/rotate_house.cpp
@@ -118,6 +118,7 @@ int main(int argc,char **argv)
 {
 	cout << "Enter rotation angle " << endl;
 	cin >> theta;
+	theta = theta * 3.14 / 180.0;
 
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
